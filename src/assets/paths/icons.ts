0ba91f22ee7ec534b5ba3dee41301096b31b5d87
Александр Арнаутов <282arni@gmail.com>
2025-09-24 17:51:47 +0300

    add

--- src/assets/paths/icons.ts
+++ src/assets/paths/icons.ts
@@ -1,1 +1,3 @@
 import '../icons/check.svg'
+import '../icons/logo.svg'
+import '../icons/arrow.svg'
--- src/assets/paths/images.ts
+++ src/assets/paths/images.ts
@@ -1,2 +1,3 @@
 import '../images/basket.png'
 import '../images/lake.jpg'
+import '../images/avatar.png'
